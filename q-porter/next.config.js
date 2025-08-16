eb2c510953c2af737212572ca458ed11bede0297
CEO <ceo@map2map.com>
2025-08-16 18:39:25 +0000

    fix build error

--- q-porter/next.config.js
+++ q-porter/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  // Export static HTML for GitHub Pages
+  output: 'export',
+  images: {
+    // Static export requires unoptimized images or external loader
+    unoptimized: true,
+  },
 }
